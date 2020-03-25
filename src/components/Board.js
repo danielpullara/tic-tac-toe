2d6d0433da68a8a006be0a63f08f9103b1cb2090
dannyp <danielpullara92@gmail.com>
2020-03-24 21:39:23 -1000

    show winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -13,10 +13,36 @@ export default class Board extends Component {
         this.props.setParentState(obj);          
         }
     }
+    checkGameIsOver(squares) {
+        const winCase = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8], 
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
+        for (let i = 0; i < winCase.length; i++) {
+          let[a, b, c] = winCase[i];
+          if (this.props.squares[a] === this.props.squares[b] && this.props.squares[a] === this.props.squares[c]) {
+            return this.props.squares[a];
+          }
+        }
+        return null;
+      }
     
     render() {  
         let status= ``;
-        status = this.props.nextPlayer ? `nextPlayer is O` : `nextPlayer is X`
+        let winner = this.checkGameIsOver();
+        if (winner){
+            status = `winner is ${winner}`
+        }else {
+            status = this.props.nextPlayer ? `nextPlayer is O` : `nextPlayer is X`
+        }
+        
+        
         return (
             <div>
                 <h2>{status}</h2>
